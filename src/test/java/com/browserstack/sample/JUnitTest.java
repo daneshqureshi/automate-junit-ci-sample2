745d6f741752fa20f649880a2534caab5f6125c1
Pulkit Sharma <avastpulkit@gmail.com>
2017-02-09 12:24:39 +0530

    Printing the url

--- src/test/java/com/browserstack/sample/JUnitTest.java
+++ src/test/java/com/browserstack/sample/JUnitTest.java
@@ -36,6 +36,8 @@ public class JUnitTest {
         String username = System.getenv("BROWSERSTACK_USERNAME");
         String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
 
+        System.out.println(String.format("https://%s:%s@hub-cloud.browserstack.com/wd/hub", username, accessKey));
+        
         driver = new RemoteWebDriver(
                 new URL(String.format("https://%s:%s@hub-cloud.browserstack.com/wd/hub", username, accessKey)), capabilities);
     }
